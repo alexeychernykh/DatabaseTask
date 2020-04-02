c139dca212dd7730fc0afdcf2323af0199aff6ee
alexey.chernykh <alexey.chernykh@travelline.ru>
2020-04-02 17:39:18 +0300

    немного кода

---
+++ BlogApp/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp
+{
+    class Post
+    {
+        public int PostId;
+        public string Title;
+        public string Body;
+        public int AuthorId;
+        public DateTime CreationDateTime;
+    }
+}
--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -1,12 +1,121 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace BlogApp
 {
     class Program
     {
+        private static string _connectionString = @"Data Source=LENOVO\SQLEXPRESS;Initial Catalog=blog-system;Integrated Security=True;Connect Timeout=30;";
+
         static void Main( string[] args )
         {
-            Console.WriteLine( "Hello World!" );
+            string command = args[ 0 ];
+
+            if ( command == "readpost" )
+            {
+                List<Post> posts = ReadPosts();
+                Console.WriteLine( JsonSerializer.Serialize( posts ) );
+            }
+            else if ( command == "insert" )
+            {
+                int createdPostId = InsertPost( 1, "TITLE", "BODY" );
+                Console.WriteLine( "Created post: " + createdPostId );
+            }
+            else if ( command == "update" )
+            {
+                UpdatePost( 1, "UPDATED TITLE" );
+            }
+        }
+
+        private static List<Post> ReadPosts()
+        {
+            List<Post> posts = new List<Post>();
+            using ( SqlConnection connection = new SqlConnection( _connectionString ) )
+            {
+                connection.Open();
+                using ( SqlCommand command = new SqlCommand() )
+                {
+                    command.Connection = connection;
+                    command.CommandText =
+                        @"SELECT
+                            [PostId],
+                            [Title],
+                            [Body],
+                            [AuthorId],
+                            [CreationDateTime]
+                        FROM Post";
+
+                    using ( SqlDataReader reader = command.ExecuteReader() )
+                    {
+                        while ( reader.Read() )
+                        {
+                            var rawReservation = new Post
+                            {
+                                PostId = Convert.ToInt32( reader[ "PostId" ] ),
+                                Title = Convert.ToString( reader[ "Title" ] ),
+                                Body = Convert.ToString( reader[ "Body" ] ),
+                                AuthorId = Convert.ToInt32( reader[ "AuthorId" ] ),
+                                CreationDateTime = Convert.ToDateTime( reader[ "CreationDateTime" ] ),
+                            };
+                            posts.Add( rawReservation );
+                        }
+                    }
+                }
+            }
+            return posts;
+        }
+
+        private static int InsertPost( int authorId, string title, string body )
+        {
+            using ( SqlConnection connection = new SqlConnection( _connectionString ) )
+            {
+                connection.Open();
+                using ( SqlCommand cmd = connection.CreateCommand() )
+                {
+                    cmd.CommandText = @"
+                    INSERT INTO [Post]
+                       ([Title],
+                        [Body],
+                        [AuthorId],
+                        [CreationDateTime]) 
+                    VALUES 
+                       (@title,
+                        @body,
+                        @authorId,
+                        @creationDateTime)
+                    SELECT SCOPE_IDENTITY()";
+
+                    cmd.Parameters.Add( "@title", SqlDbType.NVarChar ).Value = title;
+                    cmd.Parameters.Add( "@body", SqlDbType.NVarChar ).Value = body;
+                    cmd.Parameters.Add( "@authorId", SqlDbType.Int ).Value = authorId;
+                    cmd.Parameters.Add( "@creationDateTime", SqlDbType.DateTime ).Value = DateTime.Now;
+
+                    return Convert.ToInt32( cmd.ExecuteScalar() );
+                }
+            }
+        }
+
+        private static void UpdatePost( int postId, string title )
+        {
+            using ( SqlConnection connection = new SqlConnection( _connectionString ) )
+            {
+                using ( SqlCommand command = connection.CreateCommand() )
+                {
+                    command.CommandText = @"
+                        UPDATE [Post]
+                        SET [Title] = @title
+                        WHERE PostId = @postId";
+
+                    command.Parameters.Add( "@postId", SqlDbType.BigInt ).Value = postId;
+                    command.Parameters.Add( "@title", SqlDbType.NVarChar ).Value = title;
+
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
